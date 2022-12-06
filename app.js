8deb939da46a5904775408dad3e8450d6eb2eb56
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-12-06 17:43:42 -0500

    chart styling

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const site = document.getElementById('main');
 const chartType = document.getElementById('chartType');
 const data = document.getElementById('data');
 const btn = document.getElementById('submit');
+const graphContainer = document.getElementsByClassName("graphSection");
 const graph = document.getElementsByClassName('graph');
 // * Data Output Variables
 const min = document.getElementById('min');
@@ -15,11 +16,13 @@ const mode = document.getElementById('mode');
 const variance = document.getElementById('variance');
 const stDev = document.getElementById('stDev');
 // * data Arrays
+let drawChart;
 let dataArray = [];
 let yAx = [];
-let chartChoice = "";
+let chartChoice = chartType.value;
 
 // * Set & Save Theme
+site.classList.add('darkMode');
 const theme = localStorage.getItem('theme');
 if(theme === 'dark'){
     site.classList.add('darkMode');
@@ -35,66 +38,75 @@ lightMode.addEventListener('click', () =>{
     site.classList.remove('darkMode');
     localStorage.setItem('theme','light');
 });
-// * Chart Generator Form
+
+// * Chart Generator Form Events
 chartType.addEventListener('change',() =>{
     chartChoice = chartType.value;
-    return chartChoice;
 });
 data.addEventListener('change',() =>{
     dataArray.push(data.value.split(" "));
-    return dataArray;
+    btn.addEventListener('click', () => {
+        dataArray = []; 
+        yAx = [];
+        data.value = "";
+        chartType.value = '';
+    })
 });
 // * Generate Chart
 btn.addEventListener('click', (e) =>{
-    chartChoice = chartChoice;
-    dataArray = dataArray;
     e.preventDefault();
-
+    dataArray = dataArray;
+    let dataN = dataArray[0];
     for(let i = 0; i < dataArray[0].length; i++){
         yAx.push('');
     }
 
-    const drawChart = new Chart(graph, {
+    let drawChart = new Chart (graph, {
         type: chartChoice,
         data: {
             labels: yAx,
             datasets: [{
                 backgroundColor: 'hsl(0 50% 60%)',
-                hoverBackgroundColor: 'hsl(30 30% 60%)',
-                data: dataArray[0]
-            }]
+                hoverBackgroundColor: 'hsl(60 30% 60%)',
+                data: dataN,
+                borderColor:'hsl(290 50% 80%)',
+                fill: false
+            }],
         },
-        options: {
-            legend: {
-                display: true,
-                position: 'top'
-            },
-            scales: {
-                yAxes:[{
-                    gridLines:{
-                        display:false,
-                        color:'black'
-                    },
-                    ticks:{
-                        fontColor:'#000'
-                    }
-                }],
-                xAxes:[{
-                    gridLines:{
-                        display:false,
-                        color:'black'
-                    },
-                    ticks:{
-                        fontColor:'#000'
-                    }
-                }],
-            }
-        }
+        options: chartOption
     });
+
+    setOutput();
+});
+
+const chartOption ={
+    legend: {display: false},
+    scales: {
+        yAxes:[{
+            gridLines:{
+                display:false,
+                color:'black'
+            },
+            ticks:{
+                fontColor:'#000'
+            }
+        }],
+        xAxes:[{
+            gridLines:{
+                display:false,
+                color:'black'
+            },
+            ticks:{
+                fontColor:'#000'
+            }
+        }],
+    }
+};
+function setOutput(){
     min.innerHTML = Math.min(...dataArray[0]);
     max.innerHTML = Math.max(...dataArray[0]);
     mean.innerHTML = '';
     mode.innerHTML = '';
     variance.innerHTML = '';
     stDev.innerHTML = '';
-});
+}
